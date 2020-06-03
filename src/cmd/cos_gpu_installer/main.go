beae3c3c370704d56bc3ab5338013169ec50a2b5
Ke Wu <mikewu@google.com>
2020-06-03 09:01:21 -0700

    cos-gpu-installer-v2: Add main.go
    Change-Id: Ic62e67fcfa1deeac81d383787781f7732ae832f5

---
+++ src/cmd/cos_gpu_installer/main.go
@@ -0,0 +1,32 @@
+// Package main is the program entrance.
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+
+	log "github.com/golang/glog"
+	"github.com/google/subcommands"
+
+	"cmd/cos_gpu_installer/internal/commands"
+	"pkg/utils"
+)
+
+func main() {
+	// Always log to stderr for easy debugging.
+	flag.Set("alsologtostderr", "true")
+	flag.Parse()
+
+	log.Info("Checking if this is the only cos_gpu_installer that is running.")
+	utils.Flock()
+
+	subcommands.Register(subcommands.HelpCommand(), "")
+	subcommands.Register(subcommands.FlagsCommand(), "")
+	subcommands.Register(subcommands.CommandsCommand(), "")
+	subcommands.Register(&commands.InstallCommand{}, "")
+	subcommands.Register(&commands.ListCommand{}, "")
+
+	ctx := context.Background()
+	os.Exit(int(subcommands.Execute(ctx)))
+}
