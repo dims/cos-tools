9e7cea15a9587c1ad64d4b6802e9551896f7e668
Harriet Muutu <hmuutu@google.com>
2021-07-15 15:42:44 -0400

    Add USE Metrics for memory cap and storage dev I/O
    This CL collects USE Metrics for Memory capacity and Storage device I/O. It
    also contains unit tests for the necessary functions.
    
    Change-Id: I306f07f28ae8e0546ed7fd99d5a1fbe83eea506f
    Reviewed-on: https://cos-review.googlesource.com/c/cos/tools/+/19190
    Reviewed-by: Vaibhav Rustagi <vaibhavrustagi@google.com>
    Reviewed-by: Dexter Rivera <riverade@google.com>
    Tested-by: Vaibhav Rustagi <vaibhavrustagi@google.com>
    Cloud-Build: Vaibhav Rustagi <vaibhavrustagi@google.com>

--- src/pkg/nodeprofiler/profiler/components.go
+++ src/pkg/nodeprofiler/profiler/components.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	"cos.googlesource.com/cos/tools.git/src/pkg/nodeprofiler/utils"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // Component interface defines functions that can be implemented by the
@@ -39,19 +41,12 @@ type CPU struct {
 	metrics *USEMetrics
 }
 
-func sumAtoi(a []string) (int, error) {
-	var sum int
-	for _, str := range a {
-		val, err := strconv.Atoi(str)
-		if err != nil {
-			return 0, fmt.Errorf("could not convert %s to an int: %v", str, err)
-		}
-		sum += val
-	}
-	return sum, nil
-}
-
 // CollectUtilization calculates the utilization score for the CPU Component.
+// It does this by summing the time spent running non-kernel code (user time),
+// time spent running kernel code (system time), and time stolen from a vitual
+// virtual machine (steal) to get the total CPU time spent servicing work.
+// These values can be found on vmstat's 'us' (user), 'sy' (system), and 'st'
+// (steal) columns.
 func (c *CPU) CollectUtilization(outputs map[string]utils.ParsedOutput) error {
 	cmd := "vmstat"
 	parsedOutput, ok := outputs[cmd]
@@ -59,16 +54,22 @@ func (c *CPU) CollectUtilization(outputs map[string]utils.ParsedOutput) error {
 		return fmt.Errorf("missing output for vmstat")
 	}
 	us, usPresent := parsedOutput["us"]
+	if !usPresent {
+		return fmt.Errorf("missing vmstat column 'us'")
+	}
 	sy, syPresent := parsedOutput["sy"]
+	if !syPresent {
+		return fmt.Errorf("missing vmstat column 'sy'")
+	}
 	st, stPresent := parsedOutput["st"]
-	if !usPresent || !syPresent || !stPresent {
-		return fmt.Errorf("missing some vmstat columns")
+	if !stPresent {
+		return fmt.Errorf("missing vmstat column 'st'")
 	}
 	columns := [][]string{us, sy, st}
 	var total int
 	// loop over us, sy, st columns and sum their values
 	for _, column := range columns {
-		sum, err := sumAtoi(column)
+		sum, err := utils.SumAtoi(column)
 		if err != nil {
 			return err
 		}
@@ -80,13 +81,17 @@ func (c *CPU) CollectUtilization(outputs map[string]utils.ParsedOutput) error {
 }
 
 // calculateCPUCount gets the number of processors in the system.
-func calculateCPUCount(outputs map[string]utils.ParsedOutput) (int, error) {
+// It does this by getting the value lscpu's "CPU(s)" row.
+func (c *CPU) calculateCPUCount(outputs map[string]utils.ParsedOutput) (int, error) {
 	cmd := "lscpu"
 	parsedOutput, ok := outputs[cmd]
 	if !ok {
 		return 0, fmt.Errorf("missing output for lscpu")
 	}
-	val := parsedOutput["CPU(s):"]
+	val, ok := parsedOutput["CPU(s)"]
+	if !ok {
+		return 0, fmt.Errorf("missing lscpu row 'CPU(s)'")
+	}
 	count, err := strconv.Atoi(val[0])
 	if err != nil {
 		return 0, fmt.Errorf("could not convert %s to an int: %v", val[0], err)
@@ -94,7 +99,12 @@ func calculateCPUCount(outputs map[string]utils.ParsedOutput) (int, error) {
 	return count, nil
 }
 
-// CollectSaturation calcualates the saturation value for the CPU component.
+// CollectSaturation calculates the saturation value for the CPU component.
+// It does this by comparing the number of runnable processes with the number
+// of CPUs in the system. If the number of processes (running or waiting) is
+// greater than the CPU count, the CPU component is saturated. The value of
+// runnable processes is found on vmstat's 'r' column and CPU count from
+// lscpu's "CPU(s)" row.
 func (c *CPU) CollectSaturation(outputs map[string]utils.ParsedOutput) error {
 	cmd := "vmstat"
 	parsedOutput, ok := outputs[cmd]
@@ -106,14 +116,14 @@ func (c *CPU) CollectSaturation(outputs map[string]utils.ParsedOutput) error {
 		return fmt.Errorf("missing vmstat column 'r'")
 	}
 	// loop over the "r" column and sum the values
-	sum, err := sumAtoi(running)
+	sum, err := utils.SumAtoi(running)
 	if err != nil {
 		return err
 	}
 
 	num := len(running)
 	runningProcs := sum / num
-	count, err := calculateCPUCount(outputs)
+	count, err := c.calculateCPUCount(outputs)
 	if err != nil {
 		return err
 	}
@@ -142,34 +152,311 @@ func (c *CPU) CollectUSEMetrics(outputs map[string]utils.ParsedOutput) error {
 	metrics := c.metrics
 	metrics.Timestamp = time.Now()
 	start := metrics.Timestamp
+
+	var gotErr bool
 	if err := c.CollectUtilization(outputs); err != nil {
-		return fmt.Errorf("failed to collect utilization score: %v", err)
+		gotErr = true
+		log.Errorf("failed to collect utilization for CPU: %v", err)
 	}
 	if err := c.CollectSaturation(outputs); err != nil {
-		return fmt.Errorf("failed to collect saturation score: %v", err)
+		gotErr = true
+		log.Errorf("failed to collect saturation for CPU: %v", err)
 	}
 	end := time.Now()
 	metrics.Interval = end.Sub(start)
+
+	if gotErr {
+		err := "failed to collect all USE Metrics for CPU. " +
+			"Please check the logs for more information"
+		return fmt.Errorf(err)
+	}
+	return nil
+}
+
+// MemCap holds information about the Memory capacity component:
+// name and USE Metrics collected.
+type MemCap struct {
+	name    string
+	metrics *USEMetrics
+}
+
+// CollectUtilization calculates the utilization score for Memory Capacity.
+// It does this by getting the quotient of used memory (main and virtual)
+// and total memory (main and virtual). The values for main memory can be
+// found on free's "Mem" row while virtual memory stats can be found on the
+// "Swap" row. To get the used and total values for each row, free's "used"
+// and "total" columns are used.
+func (m *MemCap) CollectUtilization(outputs map[string]utils.ParsedOutput) error {
+	cmd := "free"
+	parsedOutput, ok := outputs[cmd]
+	if !ok {
+		return fmt.Errorf("missing output for free")
+	}
+	memUsed, muPresent := parsedOutput["Mem:used"]
+	if !muPresent {
+		return fmt.Errorf("missing free's Mem row and used column")
+	}
+	swapUsed, suPresent := parsedOutput["Swap:used"]
+	if !suPresent {
+		return fmt.Errorf("missing free's Swap row and used column")
+	}
+
+	memory := [][]string{memUsed, swapUsed}
+	var used int
+	for _, mem := range memory {
+		sum, err := utils.SumAtoi(mem)
+		if err != nil {
+			return err
+		}
+		used += sum
+	}
+
+	total, err := m.calculateTotalMemory(outputs)
+	if err != nil {
+		return err
+	}
+	m.metrics.Utilization = math.Round((float64(used)/float64(total))*1000) / 1000
+	return nil
+}
+
+// calculateTotalMemory calculates the total memory on the system.
+// It does this by summing the total Main and total Swap memory which
+// can be found on free's "Mem" row + "total" column, and "Swap" row +
+// "total" column.
+func (m *MemCap) calculateTotalMemory(outputs map[string]utils.ParsedOutput) (int, error) {
+	cmd := "free"
+	parsedOutput, ok := outputs[cmd]
+	if !ok {
+		return 0, fmt.Errorf("missing output for free")
+	}
+	memTotal, mtPresent := parsedOutput["Mem:total"]
+	if !mtPresent {
+		return 0, fmt.Errorf("missing free's Mem row and total column")
+	}
+	swapTotal, stPresent := parsedOutput["Swap:total"]
+	if !stPresent {
+		return 0, fmt.Errorf("missing free's Swap row and total column")
+	}
+	memory := [][]string{memTotal, swapTotal}
+	var total int
+	for _, mem := range memory {
+		sum, err := utils.SumAtoi(mem)
+		if err != nil {
+			return 0, err
+		}
+		total += sum
+	}
+	return total, nil
+}
+
+// CollectSaturation calculates the saturation value for Memory Capacity.
+// It does this by checking whether the amount of memory being swapped in
+// and out of the disks is significant. This indicates that the system is
+// low on memory and the kernel is relying heavily on pages from the swap
+// space on the disk. Here we define "significant" as the amount of swapped
+// memory amounting to roughly 10% of the total memory." The values for
+// memory swapped in and out of disks can be found on vmstat's 'si'
+// (swapped in) and 'so' (swapped to) columns.
+func (m *MemCap) CollectSaturation(outputs map[string]utils.ParsedOutput) error {
+	cmd := "vmstat"
+	parsedOutput, ok := outputs[cmd]
+	if !ok {
+		return fmt.Errorf("missing output for vmstat")
+	}
+	si, siPresent := parsedOutput["si"]
+	if !siPresent {
+		return fmt.Errorf("missing vmstat column 'si'")
+	}
+	so, soPresent := parsedOutput["so"]
+	if !soPresent {
+		return fmt.Errorf("missing vmstat column 'so'")
+	}
+	memory := [][]string{si, so}
+	var swaps int
+	for _, swap := range memory {
+		sum, err := utils.SumAtoi(swap)
+		if err != nil {
+			return err
+		}
+		swaps += sum
+	}
+	average := swaps / len(si)
+	total, err := m.calculateTotalMemory(outputs)
+	if err != nil {
+		return err
+	}
+	// ten percent of total memory
+	threshold := 0.1 * float64(total)
+	m.metrics.Saturation = float64(average) > threshold
+	return nil
+}
+
+// CollectErrors collects errors for the MemCap component.
+func (m *MemCap) CollectErrors(outputs map[string]utils.ParsedOutput) error {
+	// Not yet implemented.
+	return nil
+}
+
+// USEMetrics returns the USE Metrics for the Memory Capacity Component.
+func (m *MemCap) USEMetrics() *USEMetrics {
+	return m.metrics
+}
+
+// Name returns the name of the Memory Capacity component.
+func (m *MemCap) Name() string {
+	return m.name
+}
+
+// CollectUSEMetrics collects USE Metrics for the MemCap component.
+func (m *MemCap) CollectUSEMetrics(outputs map[string]utils.ParsedOutput) error {
+	metrics := m.metrics
+	metrics.Timestamp = time.Now()
+	start := metrics.Timestamp
+
+	var gotErr bool
+	if err := m.CollectUtilization(outputs); err != nil {
+		gotErr = true
+		log.Errorf("failed to collect utilization for Memory capacity: %v", err)
+	}
+	if err := m.CollectSaturation(outputs); err != nil {
+		gotErr = true
+		log.Errorf("failed to collect saturation for Memory capacity: %v", err)
+	}
+	end := time.Now()
+	metrics.Interval = end.Sub(start)
+
+	if gotErr {
+		err := "failed to collect all USE metrics for Memory Capacity. " +
+			"Please check the logs for more information"
+		return fmt.Errorf(err)
+	}
+	return nil
+}
+
+// StorageDevIO holds information about the Storage device I/O component:
+// name and USE Metrics collected.
+type StorageDevIO struct {
+	name    string
+	metrics *USEMetrics
+}
+
+// CollectUtilization collects the utilization score for the StorageDevIO component.
+// It does this by getting the percentage of elapsed time during which I/O requests
+// were issued to the devices. This value can be found on iostat's '%util' column.
+func (d *StorageDevIO) CollectUtilization(outputs map[string]utils.ParsedOutput) error {
+	cmd := "iostat"
+	parsedOutput, ok := outputs[cmd]
+	if !ok {
+		return fmt.Errorf("missing output for iostat")
+	}
+	util, ok := parsedOutput["%util"]
+	if !ok {
+		return fmt.Errorf("mising iostat column util")
+	}
+
+	total, err := utils.SumParseFloat(util)
+	if err != nil {
+		return err
+	}
+	average := total / float64(len(util))
+	d.metrics.Utilization = average
+	return nil
+}
+
+// CollectSaturation collects the saturation value for the StorageDevIO component.
+// It does this by comparing the average queue length of requests that were issued
+// to the device with 1. If the queue length is greater than 1, then the Storage Device
+// component is saturated. The value for the average queue length can be found on
+// iostat's 'aqu-sz' column.
+func (d *StorageDevIO) CollectSaturation(outputs map[string]utils.ParsedOutput) error {
+	cmd := "iostat"
+	parsedOutput, ok := outputs[cmd]
+	if !ok {
+		return fmt.Errorf("missig output for iostat")
+	}
+	queue, ok := parsedOutput["aqu-sz"]
+	if !ok {
+		return fmt.Errorf("mising iostat column 'aqu-sz'")
+	}
+	total, err := utils.SumParseFloat(queue)
+	if err != nil {
+		return err
+	}
+	average := total / float64(len(queue))
+	d.metrics.Saturation = average > 1
+
+	return nil
+}
+
+// CollectErrors collects errors for the Storage Device I/O component.
+func (d *StorageDevIO) CollectErrors(outputs map[string]utils.ParsedOutput) error {
+	// yet to be implemented
+	return nil
+}
+
+// USEMetrics returns the USE Metrics for the Storage Device I/O Component.
+func (d *StorageDevIO) USEMetrics() *USEMetrics {
+	return d.metrics
+}
+
+// Name returns the name of the Storage Device I/O component.
+func (d *StorageDevIO) Name() string {
+	return d.name
+}
+
+// CollectUSEMetrics collects USE Metrics for the Storage Device I/O component.
+func (d *StorageDevIO) CollectUSEMetrics(outputs map[string]utils.ParsedOutput) error {
+	metrics := d.metrics
+	metrics.Timestamp = time.Now()
+	start := metrics.Timestamp
+
+	var gotErr bool
+	if err := d.CollectUtilization(outputs); err != nil {
+		gotErr = true
+		log.Errorf("failed to collect utilization for Storage Device I/O: %v", err)
+	}
+	if err := d.CollectSaturation(outputs); err != nil {
+		gotErr = true
+		log.Errorf("failed to collect saturation for Storage Device I/O: %v", err)
+	}
+	end := time.Now()
+	metrics.Interval = end.Sub(start)
+
+	if gotErr {
+		err := "failed to collect all USE metrics for Storage Device I/O. " +
+			"Please check the logs for more information"
+		return fmt.Errorf(err)
+	}
 	return nil
 }
 
 // GenerateUSEReport generates USE Metrics for all the components
 // as well as an analysis string to help the diagnose performance issues.
-func GenerateUSEReport(components []Component, opts Options) (USEReport, error) {
+func GenerateUSEReport(components []Component, cmds []Command) (USEReport, error) {
 	useReport := USEReport{Components: components}
-	cmds := []Command{&vmstat{"vmstat"}, &lscpu{"lscpu"}}
 	outputs := make(map[string]utils.ParsedOutput)
 
 	for _, cmd := range cmds {
-		output, err := cmd.Run(opts)
+		output, err := cmd.Run()
 		if err != nil {
-			return useReport, fmt.Errorf("failed to run %s command: %v", cmd.Name(), err)
+			log.Errorf("failed to run %q command: %v", cmd.Name(), err)
+			continue
 		}
 		name := cmd.Name()
 		outputs[name] = output
 	}
+	var failed []string
 	for _, s := range components {
-		s.CollectUSEMetrics(outputs)
+		if err := s.CollectUSEMetrics(outputs); err != nil {
+			log.Errorf("failed to collect USE metrics for %q", s.Name())
+			failed = append(failed, s.Name())
+		}
+	}
+	if len(failed) != 0 {
+		err := "failed to generate USE report for %s components" +
+			"Please check the logs for more information"
+		return useReport, fmt.Errorf(err, failed)
+
 	}
 	return useReport, nil
 }
--- src/pkg/nodeprofiler/profiler/components_test.go
+++ src/pkg/nodeprofiler/profiler/components_test.go
@@ -8,13 +8,15 @@ import (
 
 func TestCollectUtilization(t *testing.T) {
 	tests := []struct {
-		name    string
-		outputs map[string]utils.ParsedOutput
-		want    float64
-		wantErr bool
+		name      string
+		component Component
+		outputs   map[string]utils.ParsedOutput
+		want      float64
+		wantErr   bool
 	}{
 		{
-			name: "simple",
+			name:      "cpu",
+			component: &CPU{"fake", &USEMetrics{}},
 			outputs: map[string]utils.ParsedOutput{
 				"vmstat": {
 					"us": {"1", "2", "7"},
@@ -25,7 +27,8 @@ func TestCollectUtilization(t *testing.T) {
 			want: 4.67,
 		},
 		{
-			name: "missing columns",
+			name:      "missing titles",
+			component: &CPU{"fake", &USEMetrics{}},
 			outputs: map[string]utils.ParsedOutput{
 				"vmstat": {
 					"us": {"1", "2", "7"},
@@ -35,19 +38,77 @@ func TestCollectUtilization(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name:    "empty outputs",
-			outputs: map[string]utils.ParsedOutput{},
+			name:      "missing commands",
+			component: &CPU{"fake", &USEMetrics{}},
+			outputs:   map[string]utils.ParsedOutput{},
+			wantErr:   true,
+		},
+		{
+			name:      "memory capacity",
+			component: &MemCap{"fake", &USEMetrics{}},
+			outputs: map[string]utils.ParsedOutput{
+				"free": {
+					"Mem:used":   {"13"},
+					"Swap:used":  {"0"},
+					"Mem:total":  {"14520"},
+					"Swap:total": {"0"},
+				},
+			},
+			want: 0.001,
+		},
+		{
+			name:      "missing titles",
+			component: &MemCap{"fake", &USEMetrics{}},
+			outputs: map[string]utils.ParsedOutput{
+				"free": {},
+			},
 			wantErr: true,
+		},
+		{
+			name:      "missing commands",
+			component: &MemCap{"fake", &USEMetrics{}},
+			outputs: map[string]utils.ParsedOutput{
+				"vmstat": {
+					"si": {"0", "0", "3"},
+					"so": {"0", "1", "5"},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name:      "storage device",
+			component: &StorageDevIO{"fake", &USEMetrics{}},
+			outputs: map[string]utils.ParsedOutput{
+				"iostat": {
+					"%util": {"4.76", "0.09"},
+				},
+			},
+			want: 2.425,
+		},
+		{
+			name:      "missing titles",
+			component: &StorageDevIO{"fake", &USEMetrics{}},
+			outputs: map[string]utils.ParsedOutput{
+				"iostat": {
+					"%idle": {"94.27"},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name:      "missing commands",
+			component: &StorageDevIO{"fake", &USEMetrics{}},
+			outputs:   map[string]utils.ParsedOutput{},
+			wantErr:   true,
 		},
 	}
 
 	for _, test := range tests {
-		fakeCPU := &CPU{name: "fakeCPU", metrics: &USEMetrics{}}
-		err := fakeCPU.CollectUtilization(test.outputs)
-		got := fakeCPU.metrics.Utilization
+		err := test.component.CollectUtilization(test.outputs)
+		got := test.component.USEMetrics().Utilization
 
 		if gotErr := err != nil; gotErr != test.wantErr {
-			t.Fatalf("CollectUtilization(%v) err %q, wantErr %v", test.outputs, err, test.wantErr)
+			t.Fatalf("CollectUtilization(%v) err %v, wantErr %t", test.outputs, err, test.wantErr)
 		}
 		if got != test.want {
 			t.Errorf("CollectUtilization(%v) = %v, want: %v", test.outputs, test.want, got)
@@ -57,42 +118,30 @@ func TestCollectUtilization(t *testing.T) {
 
 func TestCollectSaturation(t *testing.T) {
 	tests := []struct {
-		name    string
-		outputs map[string]utils.ParsedOutput
-		want    bool
-		wantErr bool
+		name      string
+		component Component
+		outputs   map[string]utils.ParsedOutput
+		want      bool
+		wantErr   bool
 	}{
 		{
-			name: "non-saturated",
+			name:      "CPU",
+			component: &CPU{"fake", &USEMetrics{}},
 			outputs: map[string]utils.ParsedOutput{
 				"vmstat": {
-					"r": {"5", "2", "2"},
+					"r": {"15", "12", "2"},
 				},
 				"lscpu": {
-					"CPU(s):": {"8"},
-				},
-			},
-		},
-		{
-			name: "saturated",
-			outputs: map[string]utils.ParsedOutput{
-				"vmstat": {
-					"r": {"10", "10", "10"},
-				},
-				"lscpu": {
-					"CPU(s):": {"8"},
+					"CPU(s)": {"8"},
 				},
 			},
 			want: true,
 		},
 		{
-			name: "wrong columns",
+			name:      "missing titles",
+			component: &CPU{"fake", &USEMetrics{}},
 			outputs: map[string]utils.ParsedOutput{
-				"vmstat": {
-					"us": {"1", "2", "7"},
-					"sy": {"0", "1", "3"},
-					"st": {"0", "0", "0"},
-				},
+				"vmstat": {},
 				"lscpu": {
 					"CPU(s):": {"8"},
 				},
@@ -100,7 +149,8 @@ func TestCollectSaturation(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name: "missing lscpu",
+			name:      "missing command",
+			component: &CPU{"fake", &USEMetrics{}},
 			outputs: map[string]utils.ParsedOutput{
 				"vmstat": {
 					"r": {"10", "10", "10"},
@@ -109,19 +159,73 @@ func TestCollectSaturation(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name:    "empty outputs",
-			outputs: map[string]utils.ParsedOutput{},
+			name:      "Memory capacity",
+			component: &MemCap{"fake", &USEMetrics{}},
+			outputs: map[string]utils.ParsedOutput{
+				"vmstat": {
+					"si": {"0", "0", "3"},
+					"so": {"0", "1", "5"},
+				},
+				"free": {
+					"Mem:total":  {"14520"},
+					"Swap:total": {"0"},
+				},
+			},
+		},
+		{
+			name:      "missing command",
+			component: &MemCap{"fake", &USEMetrics{}},
+			outputs: map[string]utils.ParsedOutput{
+				"vmstat": {
+					"si": {"0", "0", "3"},
+					"so": {"0", "1", "5"},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name:      "missing titles",
+			component: &MemCap{"fake", &USEMetrics{}},
+			outputs: map[string]utils.ParsedOutput{
+				"vmstat": {
+					"si": {"0", "0", "3"},
+					"so": {"0", "1", "5"},
+				},
+				"free": {},
+			},
+			wantErr: true,
+		},
+		{
+			name:      "Storage Device",
+			component: &StorageDevIO{"fake", &USEMetrics{}},
+			outputs: map[string]utils.ParsedOutput{
+				"iostat": {
+					"aqu-sz": {"0.04", "0.00"},
+				},
+			},
+		},
+		{
+			name:      "missing commands",
+			component: &StorageDevIO{"fake", &USEMetrics{}},
+			outputs:   map[string]utils.ParsedOutput{},
+			wantErr:   true,
+		},
+		{
+			name:      "missing titles",
+			component: &StorageDevIO{"fake", &USEMetrics{}},
+			outputs: map[string]utils.ParsedOutput{
+				"iostat": {},
+			},
 			wantErr: true,
 		},
 	}
 
 	for _, test := range tests {
-		fakeCPU := &CPU{name: "fakeCPU", metrics: &USEMetrics{}}
-		err := fakeCPU.CollectSaturation(test.outputs)
-		got := fakeCPU.metrics.Saturation
+		err := test.component.CollectSaturation(test.outputs)
+		got := test.component.USEMetrics().Saturation
 
 		if gotErr := err != nil; gotErr != test.wantErr {
-			t.Fatalf("CollectSaturation(%v) err %q, wantErr %v", test.outputs, err, test.wantErr)
+			t.Fatalf("CollectSaturation(%v) err %v, wantErr %t", test.outputs, err, test.wantErr)
 		}
 		if got != test.want {
 			t.Errorf("CollectSaturation(%v) = %v, want: %v", test.outputs, test.want, got)
