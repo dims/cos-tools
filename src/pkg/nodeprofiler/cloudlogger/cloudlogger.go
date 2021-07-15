d4a5541ef543bb6d3b0ad7a14adbf5a0f3703220
Franck <fmugisho@google.com>
2021-07-15 14:08:35 -0400

    Added functions to log report and shell cmd output
    This CL added a function to log profiler report and the output of
    arbitrary shell commands to Google Cloud Logging backend.
    
    Change-Id: I0e70428ac780ac769f4f37cc788d755408fcd31f
    Reviewed-on: https://cos-review.googlesource.com/c/cos/tools/+/18990
    Cloud-Build: GCB Service account <228075978874@cloudbuild.gserviceaccount.com>
    Reviewed-by: Dexter Rivera <riverade@google.com>
    Tested-by: Dexter Rivera <riverade@google.com>

--- src/pkg/nodeprofiler/cloudlogger/cloudlogger.go
+++ src/pkg/nodeprofiler/cloudlogger/cloudlogger.go
@@ -1,25 +1,124 @@
 // Package cloudlogger provides functionality to log text or json data to
 // Google Cloud logging backend.
-
 package cloudlogger
 
 import (
-	"errors"
+	"context"
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
 
 	"cloud.google.com/go/logging"
-	"cos.googlesource.com/cos/tools.git/src/pkg/nodeprofiler/utils"
+	"cos.googlesource.com/cos/tools.git/src/pkg/nodeprofiler/profiler"
+
+	log "github.com/sirupsen/logrus"
 )
 
-// Defines the method required to log a text string to Google Cloud logging
-// backend.
+const defaultCommandTimeout = 300 * time.Second
+
+// componentInfo contains Name and Metrics fields similar to Name and Metrics
+// fields that each profiler component has. componentInfo helps to export
+// component fields to log them to Google Cloud Logging backend.
+type componentInfo struct {
+	Name    string
+	Metrics *profiler.USEMetrics
+}
+
+// LoggerOpts contains the options supported when logging the Profiler Report
+// to Google Cloud Logging backend.
+type LoggerOpts struct {
+	// Specifies the project ID to write logs to.
+	ProjID string
+	// Specifies raw commands for which to log output.
+	Command string
+	// Specifies the number of times to run an arbitrary shell command.
+	CmdCount int
+	// Specifies the interval separating the number of times the user runs
+	// an arbitrary shell command.
+	CmdInterval time.Duration
+	// Specifies the amount of time it will take for the a raw shell command to
+	// timeout.
+	CmdTimeOut time.Duration
+	// Specifies the number of times to run the profiler.
+	ProfilerCount int
+	// Specifies the interval the profiler will run.
+	ProfilerInterval time.Duration
+	// Components on which to run profiler. It may contain CPU(s), Memory, etc.
+	Components []profiler.Component
+	// ProfilerCmds field specifies additional options needed to run the profiler
+	ProfilerCmds []profiler.Command
+}
+
+// TextLogger defines the method required to log a text string to Google Cloud
+// logging backend.
 type TextLogger interface {
 	Printf(text string, a ...interface{})
 }
 
-// Defines the method required to log anything that can be marshaled to JSON
-// to Google Cloud logging backend.
+// StructuredLogger defines the method required to log anything that can be
+// marshaled to JSON to Google Cloud logging backend and the method that blocks
+// buffered log entries until previous log entries are sent to Google Cloud
+// logging backend.
 type StructuredLogger interface {
 	Log(l logging.Entry)
+	Flush() error
+}
+
+// Validate ensures that options are correctly set. If they aren't, the method
+// returns a list of error messages encountered. To log the output of a shell
+// command, the user must specify both the command, command count and interval
+// configurations which specify the command, how often to log its output and the
+// interval in seconds. By default, the commands will timeout after 300 seconds
+// unless the user specified a different value using the commandTimeout
+// configuration. Similarly, to run the profiler, the user must specify a
+// ProfilerCount configuration if a ProfilerInterval configuration was specified.
+func (l *LoggerOpts) Validate() error {
+	// A valid project ID must be provided.
+	if l.ProjID == "" {
+		return fmt.Errorf("invalid Logger options: cannot run profiler tool if the Cloud Logging Project ID is not set")
+	}
+	// To run the profiler, the profilerCount configuration has to be set if
+	// profilerInterval configuration is set.
+	if l.ProfilerCount == 0 && l.ProfilerInterval != 0 {
+		return fmt.Errorf("invalid Logger options: cannot set ProfilerInterval if ProfilerCount is not set")
+	}
+	// if the command configuration is nil, ensure CmdCount, CmdInterval, and
+	// CmdTimeOut configurations are all 0.
+	if l.Command == "" {
+		if l.CmdCount != 0 || l.CmdInterval != 0 || l.CmdTimeOut != 0 {
+			return fmt.Errorf("invalid Logger options: CmdCount, CmdInterval and CmdTimeout should not be set if Command is not set")
+		}
+	} else {
+		// if the the command configuration was set, but the user did not specify
+		// when the command will timeout, then the CmdTimeOut configuration will be
+		// set to defaultCommandTimeout, which is 300 seconds.
+		if l.CmdTimeOut == 0 {
+			l.CmdTimeOut = defaultCommandTimeout
+		}
+		// if CmdCount config is not set, then CmdInterval must not be set.
+		if l.CmdCount == 0 && l.CmdInterval != 0 {
+			return fmt.Errorf("invalid Logger options: cannot set CmdInterval if CmdCount is not set")
+		}
+		// if the command configuration is set but no other options is set, run the
+		// command once.
+		if l.CmdCount == 0 && l.CmdInterval == 0 {
+			l.CmdCount = 1
+		}
+	}
+	return nil
+}
+
+// checkLogError returns nil if there was no error encountered while running the profiler/shell
+// commands. If there was one or more error, a summary of errors encountered will be returned.
+func checkLogError(emptyCmd bool, errArr []error) error {
+	if len(errArr) == 0 {
+		return nil
+	}
+	if emptyCmd == true {
+		return fmt.Errorf("encountered %v errors while running the profiler: %v", len(errArr), errArr)
+	}
+	return fmt.Errorf("encountered %v errors while running the profiler and shell commands: %v", len(errArr), errArr)
 }
 
 // LogText writes the string infoToLog to a logging backend by calling the
@@ -28,31 +127,136 @@ type StructuredLogger interface {
 // type *log.Logger.
 func LogText(g TextLogger, infoToLog string) error {
 	if infoToLog == "" {
-		return errors.New("logging empty string")
+		return fmt.Errorf("cannot log an empty string to Cloud Logging")
 	}
 	g.Printf(infoToLog)
 	return nil
 }
 
-// LogUSEReport writes an array of USEMetrics to a logging backend by calling
-// the `Log` method defined by the StructuredLogger interface.
-// To log a JSON Payload to Google Cloud Logging backend, pass in an instance
-// of type *logging.Logger.
-func LogUSEReport(g StructuredLogger, useReport *utils.USEReport) error {
+// logShellCmd writes a struct containing the name of an arbitrary shell
+// command and its output to a logging backend by calling the `Log` method
+// defined by the StructuredLogger interface. To Log a JSON Payload to Google
+// Cloud Logging backend, pass in an instance of type *logging.Logger.
+func logShellCommand(g StructuredLogger, cmdTimeOut time.Duration, cmd string, args ...string) error {
+	// fullCommand string includes a main command and its options.
+	// For `ps -aux` the cmd is `ps` the options are `-aux` thus the
+	// fullCommand `ps -aux`
+	var fullCommand string
+	// formating commandName to not include blank spaces.
+	if len(args) == 0 {
+		fullCommand = cmd
+	} else {
+		fullCommand = cmd + " " + strings.Join(args, " ")
+	}
+	// Timeout after cmdTimeOut seconds.
+	ctx, cancel := context.WithTimeout(context.Background(), cmdTimeOut)
+	defer cancel()
+	out, err := exec.CommandContext(ctx, cmd, args...).CombinedOutput()
+	if ctx.Err() == context.DeadlineExceeded {
+		err := "command timed out"
+		log.Error(err)
+		return fmt.Errorf("cannot log %v to Cloud Logging: %v", fullCommand, err)
+	}
+
+	if err != nil {
+		return fmt.Errorf("cannot run %v command: %v", fullCommand, err)
+	}
+	entry := logging.Entry{
+		Payload: struct {
+			CommandName   string
+			CommandOutput string
+		}{
+			CommandName:   fullCommand,
+			CommandOutput: string(out),
+		},
+		Severity: logging.Debug,
+	}
+	g.Log(entry)
+	return nil
+}
+
+// logUSEReport writes a USEReport to a logging backend by calling the `Log`
+// method defined by the StructuredLogger interface. To log a JSON Payload to
+// Google Cloud Logging backend, pass in an instance of type *logging.Logger.
+func logUSEReport(g StructuredLogger, useReport *profiler.USEReport) error {
 	if useReport == nil {
-		return errors.New("logging empty useReport")
+		return fmt.Errorf("cannot log an empty USEReport")
+	}
+	var cInfos []componentInfo
+	for _, c := range useReport.Components {
+		cInfos = append(cInfos, componentInfo{Name: c.Name(), Metrics: c.USEMetrics()})
 	}
 	entry := logging.Entry{
 		// Log anything that can be marshaled to JSON.
 		Payload: struct {
-			Components []utils.USEMetrics
+			Components []componentInfo
 			Analysis   string
 		}{
-			Components: useReport.Components,
+			Components: cInfos,
 			Analysis:   useReport.Analysis,
 		},
 		Severity: logging.Debug,
 	}
 	g.Log(entry)
 	return nil
+}
+
+// LogProfilerReport logs the output of logShellCommand and logUSEReport
+// functions to a logger of type *logging.Logger. It takes as input the logger
+// itself, and a pointer to a LoggerOpts struct that contains the shell command
+// to execute, the number of time to execute that shell command, the interval
+// between shell command executions, the time limit for shell command execution,
+// the number of time to run the profiler tool, the interval between the number
+// of times to run the profiler, and a profiler option struct that specify the
+// component to generate USEReport for as well as any options associated to that
+// component.
+func LogProfilerReport(g StructuredLogger, opts *LoggerOpts) error {
+	var emptyCmd bool
+	errArr := []error{}
+	log.Info("Validating logger options . . .")
+	if err := opts.Validate(); err != nil {
+		return err
+	}
+	log.Info("Done validating logger options.")
+	log.Info("Running Profiler . . .")
+	// Ensure logging entries are written to the cloud logging backend.
+	defer g.Flush()
+	// Run the profiler profCount times. The default value is 1 time unless user
+	// set the counter to a different number.
+	for i := 0; i < opts.ProfilerCount; i++ {
+		useReport, err := profiler.GenerateUSEReport(opts.Components, opts.ProfilerCmds)
+		if err != nil {
+			errArr = append(errArr, fmt.Errorf("cannot run profiler.GenerateUSEReport(%v) = %v", opts.Components, err))
+			continue
+		}
+		if err := logUSEReport(g, &useReport); err != nil {
+			errArr = append(errArr, err)
+			continue
+		}
+		// Delaying execution by profilerInterval seconds.
+		time.Sleep(opts.ProfilerInterval)
+	}
+	log.Info("Done running profiler.")
+	// Only log shell command if the user specified a command.
+	if len(opts.Command) == 0 {
+		emptyCmd = true
+		return checkLogError(emptyCmd, errArr)
+	}
+	emptyCmd = false
+	log.Info("Running shell command . . .")
+	// Fetching command from user input and populating the cmdArray
+	// with the main command and its flags.
+	cmdArray := strings.Split(opts.Command, " ")
+	usrMainCmd := cmdArray[0]
+	usrMainCmdFlags := cmdArray[1:]
+	for i := 0; i < opts.CmdCount; i++ {
+		if err := logShellCommand(g, opts.CmdTimeOut, usrMainCmd, usrMainCmdFlags...); err != nil {
+			errArr = append(errArr, err)
+			continue
+		}
+		// Delaying execution by cmdInterval seconds.
+		time.Sleep(opts.CmdInterval)
+	}
+	log.Infof("Done running shell command.")
+	return checkLogError(emptyCmd, errArr)
 }
--- src/pkg/nodeprofiler/cloudlogger/cloudlogger_test.go
+++ src/pkg/nodeprofiler/cloudlogger/cloudlogger_test.go
@@ -1,22 +1,181 @@
 package cloudlogger
 
 import (
+	"fmt"
+	"io/ioutil"
 	"testing"
 	"time"
 
 	"cloud.google.com/go/logging"
+	"cos.googlesource.com/cos/tools.git/src/pkg/nodeprofiler/profiler"
 	"cos.googlesource.com/cos/tools.git/src/pkg/nodeprofiler/utils"
+
 	"github.com/google/go-cmp/cmp"
 )
+
+// fakeCPU is a struct that implements the Component interface.
+type fakeCPU struct {
+	CPUName string
+	Metrics *profiler.USEMetrics
+}
+
+// CollectUtilization behavior with regards to type fakeCPU.
+func (f *fakeCPU) CollectUtilization(outputs map[string]utils.ParsedOutput) error {
+	f.Metrics.Utilization = float64(7) // an arbitrary number
+	return nil
+}
+
+// CollectSaturation behavior with regards to type fakeCPU.
+func (f *fakeCPU) CollectSaturation(outputs map[string]utils.ParsedOutput) error {
+	f.Metrics.Saturation = true
+	return nil
+}
+
+// CollectErrors behavior with regards to type fakeCPU.
+func (f *fakeCPU) CollectErrors(outputs map[string]utils.ParsedOutput) error {
+	return nil
+}
+
+// Collect USEMetrics behavior with regards to type fakeCPU.
+func (f *fakeCPU) CollectUSEMetrics(outputs map[string]utils.ParsedOutput) error {
+	metrics := f.Metrics
+	metrics.Timestamp = time.Date(2021, time.July, 21, 9, 59, 30, 0, time.UTC)
+	// setting an arbitrary start time.
+	start := metrics.Timestamp
+	if err := f.CollectUtilization(outputs); err != nil {
+		return fmt.Errorf("failed to collect utilization for CPU: %v", err)
+	}
+	if err := f.CollectSaturation(outputs); err != nil {
+		return fmt.Errorf("failed to collect saturation for CPU: %v", err)
+	}
+	// setting an arbitrary end time.
+	end := time.Date(2021, time.July, 21, 10, 3, 0, 0, time.UTC)
+	metrics.Interval = end.Sub(start)
+	return nil
+}
+
+// USEMetrics behavior with regards to type fakeCPU.
+func (f *fakeCPU) USEMetrics() *profiler.USEMetrics {
+	return f.Metrics
+}
+
+// Name behavior with regards to type CPU.
+func (f *fakeCPU) Name() string {
+	return f.CPUName
+}
+
+// fakeMemCap is a struct that implement the Component interface.
+type fakeMemCap struct {
+	MemCapName string
+	Metrics    *profiler.USEMetrics
+}
+
+// CollectUtilization behavior with regards to type fakeMemCap.
+func (f *fakeMemCap) CollectUtilization(outputs map[string]utils.ParsedOutput) error {
+	f.Metrics.Utilization = float64(7) // any arbitrary number
+	return nil
+}
+
+// CollectSaturation behavior with regards to type fakeMemCap.
+func (f *fakeMemCap) CollectSaturation(outputs map[string]utils.ParsedOutput) error {
+	f.Metrics.Saturation = true
+	return nil
+}
+
+// CollectErrors behavior with regards to type fakeMemCap.
+func (f *fakeMemCap) CollectErrors(outputs map[string]utils.ParsedOutput) error {
+	return nil
+}
+
+// Collect USEMetrics behavior with regards to type fakeMemCap.
+func (f *fakeMemCap) CollectUSEMetrics(outputs map[string]utils.ParsedOutput) error {
+	metrics := f.Metrics
+	metrics.Timestamp = time.Date(2021, time.July, 21, 9, 59, 30, 0, time.UTC)
+	// setting an arbitrary start time.
+	start := metrics.Timestamp
+	if err := f.CollectUtilization(outputs); err != nil {
+		return fmt.Errorf("failed to collect utilization for CPU: %v", err)
+	}
+	if err := f.CollectSaturation(outputs); err != nil {
+		return fmt.Errorf("failed to collect saturation for CPU: %v", err)
+	}
+	// setting an arbitrary end time.
+	end := time.Date(2021, time.July, 21, 10, 3, 0, 0, time.UTC)
+	metrics.Interval = end.Sub(start)
+	return nil
+}
+
+// USEMetrics behavior with regards to type fakeMemCap.
+func (f *fakeMemCap) USEMetrics() *profiler.USEMetrics {
+	return f.Metrics
+}
+
+// Name behavior with regards to type fakeMemCap.
+func (f *fakeMemCap) Name() string {
+	return f.MemCapName
+}
+
+// fakeStorageDevIO is a struct that implement the Component interface.
+type fakeStorageDevIO struct {
+	StorageDevIOName string
+	Metrics          *profiler.USEMetrics
+}
+
+// CollectUtilization behavior with regards to type fakeStorageDevIO.
+func (f *fakeStorageDevIO) CollectUtilization(outputs map[string]utils.ParsedOutput) error {
+	f.Metrics.Utilization = float64(7) // any arbitrary number
+	return nil
+}
+
+// CollectSaturation behavior with regards to type fakeStorageDevIO.
+func (f *fakeStorageDevIO) CollectSaturation(outputs map[string]utils.ParsedOutput) error {
+	f.Metrics.Saturation = true
+	return nil
+}
+
+// CollectErrors behavior with regards to type fakeStorageDevIO.
+func (f *fakeStorageDevIO) CollectErrors(outputs map[string]utils.ParsedOutput) error {
+	return nil
+}
+
+// Collect USEMetrics behavior with regards to type fakeStorageDevIO.
+func (f *fakeStorageDevIO) CollectUSEMetrics(outputs map[string]utils.ParsedOutput) error {
+	metrics := f.Metrics
+	metrics.Timestamp = time.Date(2021, time.July, 21, 9, 59, 30, 0, time.UTC)
+	// setting an arbitrary start time.
+	start := metrics.Timestamp
+	if err := f.CollectUtilization(outputs); err != nil {
+		return fmt.Errorf("failed to collect utilization for CPU: %v", err)
+	}
+	if err := f.CollectSaturation(outputs); err != nil {
+		return fmt.Errorf("failed to collect saturation for CPU: %v", err)
+	}
+	// setting an arbitrary end time.
+	end := time.Date(2021, time.July, 21, 10, 3, 0, 0, time.UTC)
+	metrics.Interval = end.Sub(start)
+	return nil
+}
+
+// USEMetrics behavior with regards to type fakeStorageDevIO.
+func (f *fakeStorageDevIO) USEMetrics() *profiler.USEMetrics {
+	return f.Metrics
+}
+
+// Name behavior with regards to type fakeStorageDevIO.
+func (f *fakeStorageDevIO) Name() string {
+	return f.StorageDevIOName
+}
 
 // fakeTextLogger is a struct that implements the TextLogger interface.
 type fakeTextLogger struct {
 	logged string
 }
 
-// fakeStructuredLogger is a struct that implements the StructuredLogger interface.
+// fakeStructuredLogger is a struct that implements the StructuredLogger
+// interface.
 type fakeStructuredLogger struct {
-	logged logging.Entry
+	buffer []logging.Entry
+	logged []logging.Entry
 }
 
 // Printf behavior with regards to type fakeTextLogger.
@@ -26,11 +185,40 @@ func (f *fakeTextLogger) Printf(text string, a ...interface{}) {
 
 // Log behavior with regards to type fakeStructuredLogger.
 func (f *fakeStructuredLogger) Log(entry logging.Entry) {
-	f.logged = entry
+	f.buffer = append(f.buffer, entry)
+}
+
+// Flush behavior with regards to type fakeStructuredLogger.
+func (f *fakeStructuredLogger) Flush() error {
+	f.logged = append(f.logged, f.buffer...)
+	f.buffer = []logging.Entry{}
+	return nil
+}
+
+// generateFakeProfilerOpts initializes profiler components and commands
+// and returns them.
+func generateFakeProfilerOpts() ([]profiler.Component, []profiler.Command) {
+	fCPU := &fakeCPU{
+		CPUName: "fakeCPU",
+		Metrics: &profiler.USEMetrics{},
+	}
+	fMemCap := &fakeMemCap{
+		MemCapName: "fakeMemCap",
+		Metrics:    &profiler.USEMetrics{},
+	}
+	fStorageDevIO := &fakeStorageDevIO{
+		StorageDevIOName: "fakeStorageDevIO",
+		Metrics:          &profiler.USEMetrics{},
+	}
+	// populating fake components.
+	components := []profiler.Component{fCPU, fMemCap, fStorageDevIO}
+	// populating fake commands.
+	cmds := []profiler.Command{}
+	return components, cmds
 }
 
 // For every input, the logged string must be the expected output unless the
-// the input is an empty string. In that case, nothing get logged.
+// input is an empty string. In that case, nothing gets logged.
 func TestTableLogText(t *testing.T) {
 	var tests = []struct {
 		name       string
@@ -45,7 +233,7 @@ func TestTableLogText(t *testing.T) {
 			wantErr:    false,
 		},
 		{
-			name:       "empty log",
+			name:       "empty string log",
 			input:      "",
 			wantOutput: "",
 			wantErr:    true,
@@ -59,76 +247,221 @@ func TestTableLogText(t *testing.T) {
 		// ignoring the case in which the user logged empty string.
 		if gotErr := err != nil; gotErr != test.wantErr {
 
-			t.Errorf("LogText(%v, %v) = %q, wantErr %v", f, test.input, err, test.wantErr)
+			t.Errorf("LogText(%v, %v) = %v, wantErr %t", f, test.input, err, test.wantErr)
 		}
 		if diff := cmp.Diff(test.wantOutput, f.logged); diff != "" {
-			t.Errorf("Ran LogText(%v, %v), but got mismatch between got and want (-got, +want): \n diff %s", f, test.input, diff)
+			t.Errorf("ran logText(fakeTextLogger, %+v), but got mismatch between got and want (-got, +want): \n diff %s", test.input, diff)
 		}
-
 	}
 }
-func TestTableLogUSEReport(t *testing.T) {
-	// [Begin Making of mocked out instances]
-	useMetrics := []utils.USEMetrics{
 
-		{
-			Timestamp:   time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC),
-			Interval:    time.Duration(100) * time.Millisecond,
-			Utilization: 4,
-			Saturation:  false,
-			Errors:      int64(0),
-		},
-		{
-			Timestamp:   time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC),
-			Interval:    time.Duration(100) * time.Millisecond,
+func TestTableLogProfilerReport(t *testing.T) {
+	// Retrieving testing data.
+	inputFile := "testdata/testdata.txt"
+	inputFileData, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		t.Errorf("failed to open testing input file: %v\n", err)
+	}
+	// Retrieving profiler components and commands.
+	components, cmds := generateFakeProfilerOpts()
+	useReport := profiler.USEReport{
+		Components: components,
+	}
+	var cInfos []componentInfo
+	expected := struct {
+		Metrics *profiler.USEMetrics
+	}{
+		Metrics: &profiler.USEMetrics{
+			Timestamp:   time.Date(2021, time.July, 21, 9, 59, 30, 0, time.UTC),
+			Interval:    time.Date(2021, time.July, 21, 10, 3, 0, 0, time.UTC).Sub(time.Date(2021, time.July, 21, 9, 59, 30, 0, time.UTC)),
 			Utilization: 7,
-			Saturation:  false,
-			Errors:      int64(0),
+			Saturation:  true,
+			Errors:      0,
 		},
 	}
-	analysis := "No action required"
-	useReport := &utils.USEReport{Components: useMetrics, Analysis: analysis}
-	// [End Making of mocked out instances]
-
-	var empty *utils.USEReport
+	for _, c := range useReport.Components {
+		cInfos = append(cInfos, componentInfo{Name: c.Name(), Metrics: expected.Metrics})
+	}
 	var tests = []struct {
 		name       string
-		input      *utils.USEReport
-		wantOutput logging.Entry
+		input      *LoggerOpts
+		wantOutput []logging.Entry
 		wantErr    bool
 	}{
 		{
-			name:  "non-empty json payload log",
-			input: useReport,
-			wantOutput: logging.Entry{
-				Payload: struct {
-					Components []utils.USEMetrics
-					Analysis   string
-				}{
-					Components: useReport.Components,
-					Analysis:   useReport.Analysis,
+			name: "valid logger options and non-empty json payload log.",
+			input: &LoggerOpts{
+				ProjID:           "cos-interns-playground",
+				Command:          "bash testdata/testcmd.sh",
+				CmdCount:         1,
+				CmdInterval:      0 * time.Second,
+				CmdTimeOut:       3 * time.Second,
+				ProfilerCount:    1,
+				ProfilerInterval: 0 * time.Second,
+				Components:       components,
+				ProfilerCmds:     cmds,
+			},
+			wantOutput: []logging.Entry{
+				{
+					Payload: struct {
+						Components []componentInfo
+						Analysis   string
+					}{
+						Components: cInfos,
+						Analysis:   useReport.Analysis,
+					},
+					Severity: logging.Debug,
+				}, {
+					Payload: struct {
+						CommandName   string
+						CommandOutput string
+					}{
+						CommandName:   "bash testdata/testcmd.sh",
+						CommandOutput: string(inputFileData),
+					},
+					Severity: logging.Debug,
 				},
-				Severity: logging.Debug,
 			},
 			wantErr: false,
 		},
 		{
-			name:       "empty log",
-			input:      empty,
-			wantOutput: logging.Entry{},
+			name: "multiple commands executions and multiple profiler runs non-empty json payload log.",
+			input: &LoggerOpts{
+				ProjID:           "cos-interns-playground",
+				Command:          "bash testdata/testcmd.sh",
+				CmdCount:         3,
+				CmdInterval:      0 * time.Second,
+				CmdTimeOut:       3 * time.Second,
+				ProfilerCount:    2,
+				ProfilerInterval: 0 * time.Second,
+				Components:       components,
+				ProfilerCmds:     cmds,
+			},
+			wantOutput: []logging.Entry{
+				{
+					Payload: struct {
+						Components []componentInfo
+						Analysis   string
+					}{
+						Components: cInfos,
+						Analysis:   useReport.Analysis,
+					},
+					Severity: logging.Debug,
+				}, {
+					Payload: struct {
+						Components []componentInfo
+						Analysis   string
+					}{
+						Components: cInfos,
+						Analysis:   useReport.Analysis,
+					},
+					Severity: logging.Debug,
+				}, {
+					Payload: struct {
+						CommandName   string
+						CommandOutput string
+					}{
+						CommandName:   "bash testdata/testcmd.sh",
+						CommandOutput: string(inputFileData),
+					},
+					Severity: logging.Debug,
+				}, {
+					Payload: struct {
+						CommandName   string
+						CommandOutput string
+					}{
+						CommandName:   "bash testdata/testcmd.sh",
+						CommandOutput: string(inputFileData),
+					},
+					Severity: logging.Debug,
+				}, {
+					Payload: struct {
+						CommandName   string
+						CommandOutput string
+					}{
+						CommandName:   "bash testdata/testcmd.sh",
+						CommandOutput: string(inputFileData),
+					},
+					Severity: logging.Debug,
+				},
+			},
+			wantErr: false,
+		},
+
+		{
+			name: "invalid logger options payload log: empty command with CmdCount and/or CmdInterval.",
+			input: &LoggerOpts{
+				ProjID:           "cos-interns-playground",
+				Command:          "",
+				CmdCount:         1,
+				CmdInterval:      0 * time.Second,
+				CmdTimeOut:       3 * time.Second,
+				ProfilerCount:    1,
+				ProfilerInterval: 0 * time.Second,
+				Components:       components,
+				ProfilerCmds:     cmds,
+			},
+			wantOutput: nil,
+			wantErr:    true,
+		},
+		{
+			name: "invalid logger options payload log: inconsistent CmdCount and CmdInterval.",
+			input: &LoggerOpts{
+				ProjID:           "cos-interns-playground",
+				Command:          "bash testdata/testcmd.sh",
+				CmdCount:         0,
+				CmdInterval:      3 * time.Second,
+				CmdTimeOut:       3 * time.Second,
+				ProfilerCount:    1,
+				ProfilerInterval: 0 * time.Second,
+				Components:       components,
+				ProfilerCmds:     cmds,
+			},
+			wantOutput: nil,
+			wantErr:    true,
+		},
+		{
+			name: "invalid logger options payload log: inconsistent ProfilerCount and ProfilerInterval.",
+			input: &LoggerOpts{
+				ProjID:           "",
+				Command:          "bash testdata/testcmd.sh",
+				CmdCount:         1,
+				CmdInterval:      1 * time.Second,
+				CmdTimeOut:       3 * time.Second,
+				ProfilerCount:    0,
+				ProfilerInterval: 4 * time.Second,
+				Components:       components,
+				ProfilerCmds:     cmds,
+			},
+			wantOutput: nil,
+			wantErr:    true,
+		},
+		{
+			name: "invalid logger options payload log: no project ID.",
+			input: &LoggerOpts{
+				ProjID:           "",
+				Command:          "bash testdata/testcmd.sh",
+				CmdCount:         0,
+				CmdInterval:      3 * time.Second,
+				CmdTimeOut:       3 * time.Second,
+				ProfilerCount:    1,
+				ProfilerInterval: 0 * time.Second,
+				Components:       components,
+				ProfilerCmds:     cmds,
+			},
+			wantOutput: nil,
 			wantErr:    true,
 		},
 	}
 	for _, test := range tests {
 		var f *fakeStructuredLogger = &fakeStructuredLogger{}
-		err := LogUSEReport(f, test.input)
+		err := LogProfilerReport(f, test.input)
 		if gotErr := err != nil; gotErr != test.wantErr {
-
-			t.Errorf("LogUSEReport(%v, %v) = %q, wantErr %v", f, test.input, err, test.wantErr)
+			t.Errorf("LogProfilerReport(%v, %v) = %v, wantErr %t", f, test.input, err, test.wantErr)
 		}
 		if diff := cmp.Diff(test.wantOutput, f.logged); diff != "" {
-			t.Errorf("Ran LogUSEReport(%v,%v), but got mismatch between got and want (-got, +want): \n diff %s", f.logged, test.input, diff)
-		}
+			t.Errorf("ran LogProfilerReport(fakeStructuredLogger,%+v), but got mismatch between got and want (-got, +want): \n diff %s", test.input, diff)
 
+		}
 	}
 }
