febd842b63cb94ae9011b3fcd7c83b3d16586291
Harriet Muutu <hmuutu@google.com>
2021-08-09 18:28:25 -0400

    Added unit tests for APIs in utils.go
    Added unit tests for SumAtoi and SumParseFloat in utils.go
    
    Change-Id: I1379bed470b7c8a42fb562d64d514e505a8c8abd
    Reviewed-on: https://cos-review.googlesource.com/c/cos/tools/+/20830
    Reviewed-by: Dexter Rivera <riverade@google.com>
    Tested-by: Dexter Rivera <riverade@google.com>
    Cloud-Build: Dexter Rivera <riverade@google.com>

--- src/pkg/nodeprofiler/utils/utils_test.go
+++ src/pkg/nodeprofiler/utils/utils_test.go
@@ -6,6 +6,84 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+func TestSumAtoi(t *testing.T) {
+	tests := []struct {
+		name    string
+		slice   []string
+		want    int
+		wantErr bool
+	}{
+		{
+			name:  "sum ints",
+			slice: []string{"6", "4", "5"},
+			want:  15,
+		},
+		{
+			name:    "sum floats",
+			slice:   []string{"4.5", "6.4", "2.9"},
+			wantErr: true,
+		},
+		{
+			name:  "empty slice",
+			slice: []string{},
+		},
+		{
+			name:    "illegal value in slice",
+			slice:   []string{"two", "1"},
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		got, err := SumAtoi(test.slice)
+		if gotErr := err != nil; gotErr != test.wantErr {
+			t.Fatalf("SumAtoi(%v) = err %v, wantErr %t", test.slice, err, test.wantErr)
+		}
+		if got != test.want {
+			t.Errorf("SumAtoi(%v) = %v, want: %v", test.slice, got, test.want)
+		}
+	}
+}
+
+func TestSumParseFloat(t *testing.T) {
+	tests := []struct {
+		name    string
+		slice   []string
+		want    float64
+		wantErr bool
+	}{
+		{
+			name:  "sum floats",
+			slice: []string{"6.1", "4.46", "5.3"},
+			want:  15.86,
+		},
+		{
+			name:  "sum ints",
+			slice: []string{"4", "6", "2"},
+			want:  12.0,
+		},
+		{
+			name:  "empty slice",
+			slice: []string{},
+		},
+		{
+			name:    "illegal value in slice",
+			slice:   []string{"two point 5", "1"},
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		got, err := SumParseFloat(test.slice)
+		if gotErr := err != nil; gotErr != test.wantErr {
+			t.Fatalf("SumParseFloat(%v) = err %v, wantErr %t", test.slice, err, test.wantErr)
+		}
+		if got != test.want {
+			t.Errorf("SumParseFloat(%v) = %v, want: %v", test.slice, got, test.want)
+		}
+	}
+}
+
 func TestTrimCharacter(t *testing.T) {
 	tests := []struct {
 		name      string
