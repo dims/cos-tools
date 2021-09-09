f177c511ad5d72dcda922997260d08af3d5042e9
Arnav Kansal <rnv@google.com>
2021-09-09 21:12:19 +0000

    cos-gpu-installer-v1 release v20210929
    Additionally this change adds release/staging container name in the
    release schema to allow for flexibility in naming.
    
    Change-Id: Id4b8ed9b3aed2125a7919f3af3c6ddcc7f33095b
    Reviewed-on: https://cos-review.googlesource.com/c/cos/tools/+/22390
    Reviewed-by: Robert Kolchmeyer <rkolchmeyer@google.com>
    Tested-by: Arnav Kansal <rnv@google.com>
    Cloud-Build: Arnav Kansal <rnv@google.com>

--- release/release.py
+++ release/release.py
@@ -11,18 +11,18 @@ import os
 
 def validate_config(release_config):
   for release_container in release_config:
-    for key in ["container_name", "build_commit", "release_tags"]:
+    for key in ["staging_container_name", "release_container_name", "build_commit", "release_tags"]:
       assert key in release_container, "missing {} in entry {}".format(key, release_container)
 
 def validate_gcr_path(path):
   return len(path) > len("gcr.io/") and path[:len("gcr.io/")] == "gcr.io/"
 
-def copy_container_image(src_bucket, dst_bucket, container_name, build_tag, release_tags):
+def copy_container_image(src_bucket, dst_bucket, staging_container_name, release_container_name, build_tag, release_tags):
   assert validate_gcr_path(src_bucket), "cannot use address {}, only gcr.io/ addresses are supported".format(src_bucket)
   assert validate_gcr_path(dst_bucket), "cannot use address {}, only gcr.io/ addresses are supported".format(dst_bucket)
 
-  src_path = os.path.join(src_bucket, container_name)
-  dst_path = os.path.join(dst_bucket, container_name)
+  src_path = os.path.join(src_bucket, staging_container_name)
+  dst_path = os.path.join(dst_bucket, release_container_name)
 
   for release_tag in release_tags:
     subprocess.run(["gcloud", "container", "images", "add-tag", src_path + ":" + build_tag, dst_path + ":" + release_tag, "-q"])
@@ -35,10 +35,11 @@ def verify_and_release(src_bucket, dst_bucket, release):
 
       if release:
         for release_container in release_config:
-          container_name = release_container["container_name"]
+          staging_container_name = release_container["staging_container_name"]
+          release_container_name = release_container["release_container_name"]
           build_tag = release_container["build_commit"]
           release_tags = release_container["release_tags"]
-          copy_container_image(src_bucket, dst_bucket, container_name, build_tag, release_tags)
+          copy_container_image(src_bucket, dst_bucket, staging_container_name, release_container_name, build_tag, release_tags)
 
     except yaml.YAMLError as ex:
       raise Exception("Invalid YAML config: %s" % str(ex))
