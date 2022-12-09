b9a0c2c22e0c7ce6d2f1bf27789c2b5d9c92bee9
Arnav Kansal <rnv@google.com>
2022-12-09 19:49:26 +0000

    cos_gpu_driver_builder: Add command line option to process individual configs
    BUG=b/238215395
    
    Change-Id: I3a7e8a481752143c734ff7d5f4f83f5c76a2a250
    Reviewed-on: https://cos-review.googlesource.com/c/cos/tools/+/39850
    Cloud-Build: GCB Service account <228075978874@cloudbuild.gserviceaccount.com>
    Tested-by: Arnav Kansal <rnv@google.com>
    Reviewed-by: Meena Shanmugam <meenashanmugam@google.com>

--- src/cmd/cos_gpu_driver_builder/main.go
+++ src/cmd/cos_gpu_driver_builder/main.go
@@ -14,14 +14,16 @@ import (
 )
 
 var (
-	bucket = flag.String("watcher-gcs", "", "GCS bucket to watch for unprocessed configs.")
+	configDir = flag.String("config-dir", "", "Directory containing config.textproto and metadata file that needs to be processed.")
+	bucket    = flag.String("watcher-gcs", "", "GCS bucket to watch for unprocessed configs.")
+	lookBack  = flag.Int("lookBackDays", 7, "read configs produced within the past <lookBack> days.")
 )
 
 func main() {
 	flag.Parse()
 
-	if *bucket == "" {
-		log.Fatal("empty watcher gcs dir")
+	if *bucket == "" && *configDir == "" {
+		log.Fatal("empty watcher gcs dir and config file dir")
 	}
 
 	ctx := context.Background()
@@ -30,9 +32,18 @@ func main() {
 		log.Fatal("failed to setup client for GCS:", err)
 	}
 
-	configs, err := gpuconfig.ReadConfigs(ctx, client, *bucket, 7)
-	if err != nil {
-		log.Fatal("could not read configs:", err)
+	var configs []gpuconfig.GPUPrecompilationConfig
+	if *bucket != "" { // cos_gpu_driver_builder --watcher-gcs="cos-gpu-configs"
+		configs, err = gpuconfig.ReadConfigs(ctx, client, *bucket, *lookBack)
+		if err != nil {
+			log.Fatal("could not read configs:", err)
+		}
+	} else { // cos_gpu_driver_builder --config="gs://cos-gpu-configs/2022-09-26T16:03:17-dc65ba40/"
+		config, err := gpuconfig.ReadConfig(ctx, client, *configDir)
+		if err != nil {
+			log.Fatal("could not read config:", err)
+		}
+		configs = append(configs, config)
 	}
 
 	config.ProcessConfigs(ctx, client, configs)
